9f1a922eca1d4ce5b3d6a67287c91bf51cae3cdf
ShainAliiev <shain.aliiev@gmail.com>
2022-06-02 14:13:54 -0400

    New
    New

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// =================middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //  ================= Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
@@ -22,9 +26,28 @@ app.get("/pokemon", (req, res) => {
   });
 });
 
-app.get("/pokemon/:id", (req, res) => {
-  res.send(req.params.id);
+app.get("/pokemon/new", (req, res) => {
+  res.render("New", {
+    pageTitle: "Create a new pokemon",
+    pageHeader: "Create a new pokemon",
+  });
 });
+
+app.post("/pokemon", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  // res.send(req.params.id);
+
+  res.render("Show", {
+    pageTitle: "Details",
+    pageHeader: " Gotta catch 'Em All",
+    pokemon: pokemon[req.params.id],
+  });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT} `);
